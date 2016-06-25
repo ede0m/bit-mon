40db7f24ca90072f82877f4e38c8ebdd70dcd33f
Garritt Moede <gmoede@wisc.edu>
2016-06-25 16:51:32 -0500

    Var name changes, added get best spread def

--- bitmon/trader.py
+++ bitmon/trader.py
@@ -1,24 +1,22 @@
-import os, sys
+import os, sys, json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api_call import *
 
-bc_balance = 0 # this is the current bitcoin balance accross the three markets?
-usd_balance = 0 # this is the current us dollar balance accross the three markets?
-bc_worth = 1 # our current net worth in terms of bitcoin
-usd_worth = 1 # our current net worth in terms of us dollars
+bc_assets = 0 # this is the current bitcoin balance accross the three markets?
+usd_assets = 0 # this is the current us dollar balance accross the three markets?
+bc_worth = 1 # TOTAL ASSETS (BTC and USD across all markets) represented in BTC
+usd_worth = 1 # # TOTAL ASSETS (BTC and USD across all markets) represented in USD
 
 bc_base = 0 # baseline for bc
 usd_base = 0 # baseline for us dollars
 
-bc_r = bc_balance/bc_worth # percentage of our net worth that is liquid bitcoin
-usd_r = usd_balance/usd_worth # percentage of our net worth that is liquid us dollars
+bc_r = bc_assets/bc_worth # percentage of our net worth that is liquid bitcoin
+usd_r = usd_assets/usd_worth # percentage of our net worth that is liquid us dollars
 
-spread = 0 # current spread
+
 
 
 # a combination of buy and sell can determine if we trade (do both)
-
-
 initiateTrade = {
 	"S" : "", # sell shit
 	"B" : "", # buy shit
@@ -26,10 +24,12 @@ initiateTrade = {
 }
 
 
-## We are going to need getters and setters for the balances/ratios/worths/spread
+# Returns the best spead from any market
+def get_best_spread():
+	#CB data
+	with open(CB_db.json) as data_file:
+		data = json.load(data_file)
 
-######################################################################################
-####### Begins the section where we determine if we are buying/selling ###############
 
 
 
@@ -37,35 +37,36 @@ initiateTrade = {
 
 ## This is supposed to sort of form a "filter" or "funnel" that 
 ## our data falls down and determines risk vs. return kinda
-def arbitrage():
+def trade_decision(spread):
 	buy = "" # variable controlling if we buy
 	sell = "" # variable controlling if we sell
-	if(bc_r > .4 and bc_balance >= bc_base and spread > .015):
+	
+	if(bc_r > .4 and bc_assets >= bc_base and spread > .015):
 		sell = "S"
 		# probably some logic in here to determine how much we wanna sell
 
-	if(usd_r > .4 and usd_balance >= usd_base and spread > .015):
+	if(usd_r > .4 and usd_assets >= usd_base and spread > .015):
 		buy = "B"
 		# probably some logic in here to determine how much we wanna buy
-	if(bc_r > .2 and bc_r <= .4 and bc_balance > bc_base and spread > 0.025):
+	if(bc_r > .2 and bc_r <= .4 and bc_assets > bc_base and spread > 0.025):
 		sell = "S"
 		# probs some logic shit
-	if(usd_r > .2 and usd_r <= .4 and usd_balance > usd_base and spread > 0.025):
+	if(usd_r > .2 and usd_r <= .4 and usd_assets > usd_base and spread > 0.025):
 		buy = "B"
 		# Same ol shit
-	if(bc_r < .2 and bc_balance > usd_base and spread > 0.035):
+	if(bc_r < .2 and bc_assets > usd_base and spread > 0.035):
 		sell = "S"
 		# Some shit maybe?
-	if(usd_r < .2 and usd_balance > usd_base and spread > 0.035):
+	if(usd_r < .2 and usd_assets > usd_base and spread > 0.035):
 		buy = "B"
 		# shit
 	return sell + buy
 
-def tradingAlgo():
-	buy_c('COIN-BS', 1)
+def makeTrade():
+	send_funds('COIN-BS', 'KRK', .01, 'BTC')
 	
 
-#tradingAlgo()
+makeTrade()
 
 #initiateTrade[arbitrage()]
 
