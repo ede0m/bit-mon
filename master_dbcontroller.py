083d44e5425690e3082f49f316c6b2a0203e0f2c
Cormick Hnilicka <Cormick@wifi-946.cs.wisc.edu>
2016-06-30 15:43:39 -0500

    master db working

--- master_dbcontroller.py
+++ master_dbcontroller.py
@@ -21,8 +21,48 @@ def getSnaps():
 		print(json.dumps(p_db, indent=4, sort_keys=True))
 
 
+def getLast(exchange):
+    snapshots_keys = list(db[exchange]['snapshots'].keys())
+    last_key = snapshots_keys[0]
+    for i in snapshots_keys:
+    	tempkey = last_key.split('/')
+    	temp = i.split("/")
+    	if(temp[0] >= tempkey[0] and temp[1] > tempkey[1]):
+    		last_key = i
+    print(snapshots_keys)
+    
+    print(last_key)
+    for key in snapshots_keys:   
+        sp = key.split("/")[1]
+        if(int(last_key.split('/')[1]) < int(sp)):
+            last_ky = key
+    last_raw_data_keys = list(db[exchange]['snapshots'][last_key].keys())
+    last_raw_data_key = last_raw_data_keys[0]
+    for key in last_raw_data_keys:
+        #print("key ---> ", key)
+        #print("last_raw_data_key ----> ", last_raw_data_key)
+        mn = key.split("-")[0]
+        sc = key.split("-")[1]
+        #print("int(last_raw_data_key.split('-')[0]) <= int(mn) and int(last_raw_data_key.split('-')[1]) < int(sc) ------- ", int(last_raw_data_key.split('-')[0]) <= int(mn) and int(last_raw_data_key.split('-')[1]) < int(sc), int(last_raw_data_key.split('-')[0]), int(mn), int(last_raw_data_key.split('-')[1]),  int(sc))
+        if(int(last_raw_data_key.split('-')[0]) <= int(mn)):
+            last_raw_data_key = key
+            for ky in last_raw_data_keys:
+                minu = ky.split("-")[0]
+                sec = ky.split("-")[1]
+                if (int(last_raw_data_key.split('-')[0]) <= int(minu) and int(last_raw_data_key.split('-')[1]) < int(sec)):
+                    last_raw_data_key = ky
+    # print(last_raw_data_key)
+    last_raw_data = db[exchange]['snapshots'][last_key][last_raw_data_key]
+    # print(last_raw_data)
+    last = last_raw_data["last"]
+    last_split = last.split("--")
+    last_num = last_split[0]
+    print(last_num)
+    return last_num
+
+
 load()
-getSnaps("KRK")
+getLast("KRK")
 
 
 	
